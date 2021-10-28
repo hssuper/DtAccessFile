a279ae038f01021ffeb51d54df1f33854325a658
hssuper <higorsouza2306@gmail.com>
2021-10-28 10:42:56 -0300

    Adicionar arquivos de projeto.

---
+++ DtAccessFile/DtAccessFile/App.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DtAccessFile
+{
+    public partial class App : Application
+    {
+        private const string EMAILKEY = "Email";
+        private const string ENVIAEMAILKEY = "EnviaEmail";
+
+        public string Email
+        {
+            get
+            {
+                try
+                {
+                    if (Properties.ContainsKey(EMAILKEY))
+                        return Properties[EMAILKEY].ToString();
+                    return "";
+                }
+                catch (Exception erro)
+                {
+                    return "Properties[EMAILKEY] vazia. \nErro: " + erro.ToString();
+                }
+            }
+            set
+            {
+                Properties[EMAILKEY] = value;
+            }
+        }
+
+        public bool EnviaEmail
+        {
+            get
+            {
+                try
+                {
+                    if (Properties.ContainsKey(ENVIAEMAILKEY))
+                        return (bool)Properties[ENVIAEMAILKEY];
+                    return false;
+                }
+                catch
+                {
+                    return false;
+                }
+
+            }
+            set
+            {
+                Properties[ENVIAEMAILKEY] = value;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DtAccessFile/DtAccessFile/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DtAccessFile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var app = Application.Current as App;
+            app.Email = TxtEmail.Text;
+            app.EnviaEmail = SwitchEnviaEmail.On;
+            // se quiser salvar imediatamente
+            Application.Current.SavePropertiesAsync();
+            if (app.EnviaEmail == true)
+            {
+                if (app.Email != "")
+                {
+                    LabelEmail.Text = app.Email;
+                }
+                else
+                {
+                    LabelEmail.Text = "Properties Email vazia";
+                }
+            }
+            else
+            {
+                LabelEmail.Text = "";
+            }
+        }
+    }
+}
